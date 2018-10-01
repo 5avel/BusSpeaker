f39d9e50d4e5e7597db8d1e5db4ede23b37ef17c
Bolubash Pavel <5avel@hotmail.com>
2018-10-01 10:26:58 +0300

    Fix Model

---
+++ BusSpeaker/BusSpeaker/Models/Point.cs
@@ -0,0 +1,12 @@
+﻿namespace BusSpeaker.Models
+{
+    public class Point
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Sound { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public bool IsVisited { get; set; }
+    }
+}
--- BusSpeaker/BusSpeaker/Models/Rout.cs
+++ BusSpeaker/BusSpeaker/Models/Rout.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-
+﻿using System.Collections.Generic;
 namespace BusSpeaker.Models
 {
     public class Rout
@@ -9,18 +7,7 @@ namespace BusSpeaker.Models
         public string Name { get; set; }
         public string Description { get; set; }
         public List<Point> DirectDirectionPoints { get; set; }
-
         public List<Point> ReverseDirectionPoints { get; set; }
         public bool IsDirectDirection { get; set; }
-    }
-
-    public class Point
-    {
-        public string Id { get; set; }
-        public string Name { get; set; }
-        public string Sound { get; set; }
-        public double Latitude { get; set; }
-        public double Longitude { get; set; }
-        public bool IsVisited { get; set; }
     }
 }
