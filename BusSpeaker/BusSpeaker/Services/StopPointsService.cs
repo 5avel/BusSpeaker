8351cbaed0548e8fce3de135246fa53f0e914b18
Bolubash Pavel <5avel@hotmail.com>
2019-05-24 09:13:46 +0300

    Refactoring StopPointsService

--- BusSpeaker/BusSpeaker/Services/StopPointsService.cs
+++ BusSpeaker/BusSpeaker/Services/StopPointsService.cs
@@ -3,22 +3,19 @@ using BusSpeaker.Models;
 using BusSpeaker.Services.Intefaces;
 using Plugin.Geolocator.Abstractions;
 using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
 
 namespace BusSpeaker.Services
 {
     public class StopPointsService : IStopPointsService
     {
-        private IRoutRepository _repository;
-        private IGeolocatorService _geolocator;
-        private ISettingsRepository _settings;
-        private IStopsSoundService _soundService;
+        private readonly IRoutRepository _repository;
+        private readonly ISettingsRepository _settings;
+        private readonly IStopsSoundService _soundService;
 
-        private Rout currentRout;
-        private bool currentDirection;
+        private Rout _currentRout;
+        private bool _currentDirection;
 
         public event EventHandler<EventArgs> DirectionChanged;
         public ObservableCollection<StopPoint> StopPoints { get; set; }
@@ -26,32 +23,31 @@ namespace BusSpeaker.Services
         public StopPointsService(IRoutRepository repository, IGeolocatorService geolocator, ISettingsRepository settings, IStopsSoundService soundService)
         {
             _repository = repository;
-            _geolocator = geolocator;
             _settings = settings;
             _soundService = soundService;
             StopPoints = new ObservableCollection<StopPoint>();
-            _geolocator.BusPositionChanged += _geolocator_BusPositionChanged;
+            geolocator.BusPositionChanged += _geolocator_BusPositionChanged;
             LoadRout();
             ChangeDirection(true);
         }
 
         private void LoadRout()
         {
-            currentRout = _repository.GetRoutById(_settings.GetSettings().CurrentRoutId);
+            _currentRout = _repository.GetRoutById(_settings.GetSettings().CurrentRoutId);
         }
 
         public void ChangeDirection(bool isDirectDirection)
         {
-            currentDirection = isDirectDirection;
+            _currentDirection = isDirectDirection;
             StopPoints.Clear();
-            foreach (var item in currentRout.StopPoints.Where(x => x.IsDirectDirection == currentDirection))
+            foreach (var item in _currentRout.StopPoints.Where(x => x.IsDirectDirection == _currentDirection))
             {
                 item.State = 0;
                 StopPoints.Add(item);
             }
         }
 
-        private void _geolocator_BusPositionChanged(object sender, Plugin.Geolocator.Abstractions.PositionEventArgs e)
+        private void _geolocator_BusPositionChanged(object sender, PositionEventArgs e)
         {
             if (StopPoints == null) return; // не задано направление
 
@@ -65,7 +61,7 @@ namespace BusSpeaker.Services
                 {
                     if(StopPoints[i].State.HasFlag(StopPointState.Visited) == false)
                     {
-                        _soundService.PlaySound($"{currentRout.Name}.{StopPoints[i].Sound}");
+                        _soundService.PlaySound($"{_currentRout.Name}.{StopPoints[i].Sound}");
                     }
 
                     StopPoints[i].State = StopPointState.Visited | StopPointState.Current;
